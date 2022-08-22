9baddc2925034b29c33ea52e3f6840be940c3083
malikajusupova <malikajusupova0@gmail.com>
2022-08-22 13:29:55 +0600

    dfg

--- src/Routing.js
+++ src/Routing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Autorization from "./components/Autorization/Autorization";
+import Carousel from "./components/Carousel/Carousel";
 import Enter from "./components/Enter/Enter";
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./pages/HomePage/Homepage";
@@ -9,6 +10,7 @@ const Routing = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <Carousel />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/enter" element={<Enter />} />
